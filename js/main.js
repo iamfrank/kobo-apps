9ba56a26f8989e4d0367984ed41e10298bc8a7b8
iamfrank <frank@magenta.dk>
2020-09-30 18:23:03 +0200

    test

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 // Script is targeted towards Android 4 browser
 
-const showContent = function() {
+function showContent() {
     document.getElementById("test").style.display = 'block'
 }
+
+function showConsole(msg) {
+    var consl = document.getElementById("console")
+    consl.innerHTML = console + '<br>' + msg
+}
+
+showConsole(stuff)
