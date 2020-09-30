b9e2becebdcfce13af891445efc52be843059363
iamfrank <frank@magenta.dk>
2020-09-30 19:37:36 +0200

    babelify code

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
-// Script is targeted towards Android 4 browser
+let idx = 0,
+    o = 3,
+    set_color = 'green'
 
-function showContent() {
+let colorpicker_template =  `
+    <div class="colorpicker">
+        <button :class="colorpicker--btn colorpicker--btn-${ idx } colorpicker--btn-set${ set_color }" onclick="showColorPicker()">${ set_color }</button>
+        <div class="colorpicker--menu" v-if="colorpicker_visible">
+            <div v-for="o in options" class="colorpicker--pick">
+                <span :class="colorpicker--pick-${ o }" onclick="pickColor(o)">${ o }</span>
+            </div>
+        </div>
+    </div>
+`
+
+const showContent = function() {
     document.getElementById("test").style.display = 'block'
+    
+    document.body.appendChild(colorpicker_template);
+  
 }
-
-function showConsole(msg) {
-    var consl = document.getElementById("console")
-    consl.innerHTML = console + '<br>' + msg
-}
-
-showConsole(stuff)
